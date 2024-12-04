e7d2ba4726c1f6205e354950eae0396fd3262e15
원이 <163308981+circle0802@users.noreply.github.com>
2024-12-04 09:11:25 +0900

    Add files via upload

---
+++ Tuist/Tuist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tuist
+//
+//  Created by 원이 on 12/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Tuist/TuistTests/TuistTests.swift
@@ -0,0 +1,17 @@
+//
+//  TuistTests.swift
+//  TuistTests
+//
+//  Created by 원이 on 12/4/24.
+//
+
+import Testing
+@testable import Tuist
+
+struct TuistTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
